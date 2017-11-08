d859fffdf37a6615390fa43be1c3b67ec9faec4d
Songsungeun <sesong@ovncorp.com>
2017-11-08 15:29:38 +0900

    [Admin] 어드민 페이지 Required Board 작업중

--- src/main/webapp/lib/js/admin/admin.js
+++ src/main/webapp/lib/js/admin/admin.js
@@ -1,11 +1,20 @@
 /**
  * 
  */
+
+var source_user;
+var source_required;
 $(document).ready(function() {
-	
 	tabsFunc();
 	ajaxGetUser();
 })
+
+window.onload = function() {
+	source_user = $('#user_list').html();
+	source_required = $('#required_list').html();
+	
+	console.log("source_user : " + source_user);
+}
 
 function tabsFunc() {
 
@@ -13,7 +22,17 @@ function tabsFunc() {
         $("ul.tabs li").removeClass("active").css("color", "#333");
         $(this).addClass("active").css("color", "white");
         var activeTab = $(this).attr("value");
-        console.log("activeTab = " + activeTab);
+        var target = '.' + $(this).attr("data-target");
+        console.log("activeTab1 = " + activeTab);
+        
+        var url;
+        if (target == '.user_tab') {
+        	url = "../user/userList.json";
+        	ajaxGetList(url, target, null);
+        } else {
+        	url = "../board/getAdminRequiredBoard.json";
+        	ajaxGetList(url, target, 'notice');
+        }
     });
 }
 
@@ -29,17 +48,46 @@ function ajaxGetUser() {
 				alert("데이터 로드 실패");
 			} else {
 				console.log(result);
-				showUserList(result);
+				showList(result, $('#user_list').html(), '.user_tab');
 			}
 		}
 	})
 }
 
-function showUserList(result) {
-	var source = $('#user_list').html();
+function ajaxGetList(url, source, target, category) {
+	var source;
+	if (category != null) {
+		console.log("category 있음 필독 보드")
+		source = source_user;
+	} else {
+		console.log("category null 유저리스트");
+		source = source_required; 
+	}
+	
+	console.log("source_user: " + source_user);
+	
+	$.ajax({
+		url: url,
+		type: "GET",
+		success : function(obj) {
+			var result = obj.jsonResult;
+			if (result.state != "success") {
+				alert("데이터 로드 실패");
+			} else {
+				console.log(result);
+				showList(result, source, target);
+			}
+		}
+			
+	})
+}
+
+function showList(result, source, target) {
+	console.log("source : " + source);
+//	var source = $('#user_list').html();
 	var template = Handlebars.compile(source);
 	var html = template(result);
-	$('.user_tab').append(html);
+	$(target).html(html);
 }
 
 function approveUser(userNo) {
