225df1005ac89fbbd4bd2b5352d029b4ebb6ab41
Songsungeun <sesong@ovncorp.com>
2017-11-08 18:41:52 +0900

    [Admin] 사용자와 게시물 페이지 분할

--- src/main/webapp/lib/js/admin/admin.js
+++ src/main/webapp/lib/js/admin/admin.js
@@ -2,39 +2,10 @@
  * 
  */
 
-var source_user;
-var source_required;
 $(document).ready(function() {
-	tabsFunc();
 	ajaxGetUser();
 })
 
-window.onload = function() {
-	source_user = $('#user_list').html();
-	source_required = $('#required_list').html();
-	
-	console.log("source_user : " + source_user);
-}
-
-function tabsFunc() {
-
-    $("ul.tabs li").click(function () {
-        $("ul.tabs li").removeClass("active").css("color", "#333");
-        $(this).addClass("active").css("color", "white");
-        var activeTab = $(this).attr("value");
-        var target = '.' + $(this).attr("data-target");
-        console.log("activeTab1 = " + activeTab);
-        
-        var url;
-        if (target == '.user_tab') {
-        	url = "../user/userList.json";
-        	ajaxGetList(url, target, null);
-        } else {
-        	url = "../board/getAdminRequiredBoard.json";
-        	ajaxGetList(url, target, 'notice');
-        }
-    });
-}
 
 function ajaxGetUser() {
 	var url = "../user/userList.json";
@@ -54,33 +25,6 @@ function ajaxGetUser() {
 	})
 }
 
-function ajaxGetList(url, source, target, category) {
-	var source;
-	if (category != null) {
-		console.log("category 있음 필독 보드")
-		source = source_user;
-	} else {
-		console.log("category null 유저리스트");
-		source = source_required; 
-	}
-	
-	console.log("source_user: " + source_user);
-	
-	$.ajax({
-		url: url,
-		type: "GET",
-		success : function(obj) {
-			var result = obj.jsonResult;
-			if (result.state != "success") {
-				alert("데이터 로드 실패");
-			} else {
-				console.log(result);
-				showList(result, source, target);
-			}
-		}
-			
-	})
-}
 
 function showList(result, source, target) {
 	console.log("source : " + source);
